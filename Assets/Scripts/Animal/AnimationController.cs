d51edff5cee859e804faeea53d0c91661871ea26
longnt <longnt@rocketstudio.com.vn>
2023-03-27 12:00:31 +0700

    first commit

---
+++ Assets/Scripts/Animal/AnimationController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Animal
+{
+    public class AnimationController : MonoBehaviour
+    {
+		#region AnimationHash
+		private static readonly int attackHash = Animator.StringToHash("Attack");
+		private static readonly int bounceHash = Animator.StringToHash("Bounce");
+		private static readonly int clickedHash = Animator.StringToHash("Clicked");
+		private static readonly int deathHash = Animator.StringToHash("Death");
+		private static readonly int eatHash = Animator.StringToHash("Eat");
+		private static readonly int fearHash = Animator.StringToHash("Fear");
+		private static readonly int flyHash = Animator.StringToHash("Fly");
+		private static readonly int hitHash = Animator.StringToHash("Hit");
+		private static readonly int idleAHash = Animator.StringToHash("Idle_A");
+		private static readonly int idleBHash = Animator.StringToHash("Idle_B");
+		private static readonly int idleCHash = Animator.StringToHash("Idle_C");
+		private static readonly int jumpHash = Animator.StringToHash("Jump");
+		private static readonly int rollHash = Animator.StringToHash("Roll");
+		private static readonly int runHash = Animator.StringToHash("Run");
+		private static readonly int sitHash = Animator.StringToHash("Sit");
+		private static readonly int spinHash = Animator.StringToHash("Spin");
+		private static readonly int swimHash = Animator.StringToHash("Swim");
+		private static readonly int walkHash = Animator.StringToHash("Walk");     
+		#endregion
+	}
+
+}
