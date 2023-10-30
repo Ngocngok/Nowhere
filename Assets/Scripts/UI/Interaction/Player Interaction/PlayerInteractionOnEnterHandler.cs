b699400710d2eea7f4acbdd9e8a49c082512d05d
Ngocngok <hackerdaongbo@gmail.com>
2023-10-30 11:32:06 +0700

    l

--- Assets/Scripts/UI/Interaction/Player Interaction/PlayerInteractionOnEnterHandler.cs
+++ Assets/Scripts/UI/Interaction/Player Interaction/PlayerInteractionOnEnterHandler.cs
@@ -1,18 +1,72 @@
+using Nowhere.Interaction;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
-public class PlayerInteractionOnEnterHandler : MonoBehaviour
+public class PlayerInteractionOnEnterHandler : MonoBehaviour, IPlayerInteractionHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInteractionPopup _playerInteractionPopup;
+    private ComponentRepository _componentRepository;
+    private CharacterDetection _characterDetection;
+
+    public void Initialize(PlayerInteractionPopup popup)
     {
-        
+        _playerInteractionPopup = popup;
+        _componentRepository = GetComponentInParent<ComponentRepository>();
+        _characterDetection = _componentRepository.GetCachedComponent<CharacterDetection>();
+
+
+        _characterDetection.OnCharacterEnterObservable.Subscribe(OnStartInteraction);
+        _characterDetection.OnCharacterExitObservable.Subscribe(OnStopInteraction);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnStartInteraction(CharacterBehavior character)
     {
-        
+        if (character is not PlayerBehavior)
+        {
+            return;
+        }
+
+        IInteracter[] interacters = character.GetComponent<ComponentRepository>().GetCachedComponents<IInteracter>();
+
+        foreach (KeyValuePair<IInteractable, PlayerInteractionButton> interactionButton in _playerInteractionPopup.InteractionButtonDictionary)
+        {
+            bool foundCompatibleInteracter = false;
+            IInteractable interactable = interactionButton.Key;
+
+            foreach (IInteracter interacter in interacters)
+            {
+                if (interactionButton.Key.IsCompatibleWithInteracter(interacter))
+                {
+                    interactionButton.Value.Setup(_playerInteractionPopup, interactable, interacter);
+                    interactionButton.Value.Init(true, interactable.IsInteractable(interacter));
+                    foundCompatibleInteracter = true;
+
+                    break;
+                }
+            }
+
+            if (!foundCompatibleInteracter)
+            {
+                interactionButton.Value.Init(false);
+            }
+        }
+
+        _playerInteractionPopup.ShowAllActiveButton();
     }
+
+
+
+    private void OnStopInteraction(CharacterBehavior character)
+    {
+        if (character is not PlayerBehavior)
+        {
+            return;
+        }
+
+        _playerInteractionPopup.HideAllActiveButton();
+    }
+
 }
--- Assets/Scripts/UI/Interaction/Player Interaction/PlayerInteractionPopup.cs
+++ Assets/Scripts/UI/Interaction/Player Interaction/PlayerInteractionPopup.cs
@@ -11,6 +11,8 @@ public class PlayerInteractionPopup : MonoBehaviour
     private ComponentRepository _componentRepository;
     private CharacterDetection _characterDetection;
     private PlayerBehavior _playerBehavior;
+    private IPlayerInteractionHandler _playerInteractionHandler;
+
 
     private readonly Dictionary<IInteractable, PlayerInteractionButton> _interactionButtonDictionary = new();
     public IReadOnlyDictionary<IInteractable, PlayerInteractionButton> InteractionButtonDictionary => _interactionButtonDictionary;
@@ -19,14 +21,17 @@ public class PlayerInteractionPopup : MonoBehaviour
     {
         _componentRepository= GetComponentInParent<ComponentRepository>();
         _characterDetection = _componentRepository.GetCachedComponent<CharacterDetection>();
+        _playerInteractionHandler = _componentRepository.GetCachedComponent<IPlayerInteractionHandler>();
     }
 
     private void Start()
     {
         GenerateButton();
 
-        _characterDetection.OnCharacterEnterObservable.Subscribe(OnPlayerEnter);
-        _characterDetection.OnCharacterExitObservable.Subscribe(OnPlayerExit);
+        _playerInteractionHandler.Initialize(this);
+
+        //_characterDetection.OnCharacterEnterObservable.Subscribe(OnStartInteraction);
+        //_characterDetection.OnCharacterExitObservable.Subscribe(OnStopInteraction);
     }
 
     private void GenerateButton()
@@ -40,57 +45,6 @@ public class PlayerInteractionPopup : MonoBehaviour
         }
     }
 
-    private void OnPlayerEnter(CharacterBehavior character)
-    {
-        if(character is not PlayerBehavior)
-        {
-            return;
-        }
-
-        if(_playerBehavior == null)
-        {
-            _playerBehavior = character as PlayerBehavior;
-        }
-
-        IInteracter[] interacters = character.GetComponent<ComponentRepository>().GetCachedComponents<IInteracter>();
-
-        foreach (KeyValuePair<IInteractable, PlayerInteractionButton> interactionButton in _interactionButtonDictionary)
-        {
-            bool foundCompatibleInteracter = false;
-            IInteractable interactable = interactionButton.Key;
-
-            foreach (IInteracter interacter in interacters)
-            {
-                if (interactionButton.Key.IsCompatibleWithInteracter(interacter))
-                {
-                    interactionButton.Value.Setup(this, interactable, interacter);
-                    interactionButton.Value.Init(true, interactable.IsInteractable(interacter));
-                    foundCompatibleInteracter = true;
-
-                    break;
-                }
-            }
-
-            if (!foundCompatibleInteracter)
-            {
-                interactionButton.Value.Init(false);
-            }
-        }
-
-        ShowAllActiveButton();
-    }
-
-
-
-    private void OnPlayerExit(CharacterBehavior character)
-    {
-        if (character is not PlayerBehavior)
-        {
-            return;
-        }
-
-        HideAllActiveButton();
-    }
 
     public void ShowAllActiveButton(bool withAnimation = true)
     {
